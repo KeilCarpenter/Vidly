78d640aae09c91d4678cbc4c4239bc91bf9b0a15
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-01-17 00:49:50 +1300

    populate MembershipTypes table

---
+++ Vidly/Migrations/201701161125247_AddAnnotationToCustomerName.cs
@@ -0,0 +1,28 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAnnotationToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+            DropTable("dbo.Movies");
+        }
+    }
+}
