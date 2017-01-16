3ae7d023ea5aa84e2b2723c656714987fbbe03da
Keil Carpenter <Keil Carpenter>
2017-01-16 21:58:05 +1300

    Display Movie details in ViewModel

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "The Lord of The Rings" },
+                new Movie { Id = 2, Name = "The Hobbit" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "The Hobbit" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Keil Carpenter" },
+                new Customer { Name = "Clarissa Walsh" }
+            };
+
+            var viewModel = new MovieCustomerViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
