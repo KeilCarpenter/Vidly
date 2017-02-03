2034ae434ed8a2a8b9eab3ec3bf6d98b44d5c30d
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-02-04 08:43:00 +1300

    validation summary on CustomerForm

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -50,6 +50,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             
